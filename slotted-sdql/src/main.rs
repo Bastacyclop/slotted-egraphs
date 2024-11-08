ac9fd5301e6b5680c7dfc408e46a78575d75bbb0
Amir Shaikhha <amir.shaikhha@ed.ac.uk>
2024-11-08 13:50:12 +0000

    minor improvement in reporting

--- slotted-sdql/src/main.rs
+++ slotted-sdql/src/main.rs
@@ -36,6 +36,6 @@ fn main() {
     println!("  Egraph size: {} nodes, {} classes", report.egraph_nodes, report.egraph_classes);
     println!("  Total time: {}", report.total_time);
     println!("{} & {} & {} & {}", report.iterations, report.egraph_nodes, report.egraph_classes, 
-        if matches!(report.stop_reason, slotted_egraphs::StopReason::Saturated) { "Yes" } else { "No" } )
+        if matches!(report.stop_reason, slotted_egraphs::StopReason::Saturated) { "\\yes" } else { "\\no" } )
     // may_trace_assert_reaches(lhs, rhs, csv_f, 60);
 }
